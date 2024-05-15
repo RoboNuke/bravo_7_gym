fa8aa66b05803d99802eed5452f09bb463c56a47
RoboNuke <brownhun@oregonstate.edu>
2024-05-15 09:07:52 -0700

    Added new envs to gymnasium

--- __init__.py
+++ __init__.py
@@ -6,3 +6,14 @@ register(
     max_episode_steps=100,
 )
 
+register(
+    id="Bravo7_DenseRepo-v0",
+    entry_point="bravo_7_gym.envs.repo_dense:Bravo7RepoDense",
+    max_episode_steps=100,
+)
+
+register(
+    id="Bravo7FixedPegInsert-v0",
+    entry_point="bravo_7_gym.envs.fixed_peg_insert:Bravo7FixedPegInsert",
+    max_episode_steps=100,
+)
